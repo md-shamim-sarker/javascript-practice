2edc64d36905dc954e9dbdc851cc581186bf973d
shamimspro <shamimspro@gmail.com>
2023-01-21 10:40:38 +0600

    complete practice on data types

---
+++ data_type.js
@@ -0,0 +1,24 @@
+const age = 33;
+console.log(typeof age);
+
+const fullName = 'Shamim Sarker';
+console.log(typeof fullName);
+
+const isGood = true;
+console.log(typeof isGood);
+
+let undefinedVariable;
+console.log(typeof undefinedVariable);
+
+let nullVariable = null;
+console.log(typeof nullVariable);
+
+const array = [50, 60, 70];
+console.log(typeof array);
+console.log(Array.isArray(array));
+
+const object = {
+    brand: 'Nokia',
+    price: 40000
+};
+console.log(typeof object);
