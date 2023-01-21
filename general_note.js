470e1932482c41bb81010fc850789dfc572b39c6
shamimspro <shamimspro@gmail.com>
2023-01-21 09:45:52 +0600

    javascript variable 'var'

---
+++ general_note.js
@@ -0,0 +1,5 @@
+/**
+ * JavaScript is the world's most popular programming language.
+ * JavaScript is the programming language of the Web.
+ * JavaScript is a dynamic programming language that's used for web development, in web applications, for game development, and lots more. It allows us to implement dynamic features on web pages that cannot be done with only HTML and CSS.
+ */
---
+++ variables.js
@@ -0,0 +1,21 @@
+/**
+ * Variable are containers for storing data. We can use var, let and const keywords to declare variable in JavaScript.
+ */
+
+{
+    var x = 10;
+    console.log(x);
+
+    var x = 20;
+    console.log(x);
+    // Re-declaration allowed
+
+    x = 30;
+    console.log(x);
+    // Re-assignation allowed
+}
+
+{
+    let y = 40;
+
+}
