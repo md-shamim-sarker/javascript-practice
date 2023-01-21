567dd8c18e73f5fa7b51bc2c59acb25301c6558f
shamimspro <shamimspro@gmail.com>
2023-01-21 10:19:53 +0600

    complete practice on variables

--- variables.js
+++ variables.js
@@ -2,20 +2,44 @@
  * Variable are containers for storing data. We can use var, let and const keywords to declare variable in JavaScript.
  */
 
+// {
+//     var x = 10;
+//     console.log(x);
+
+//     var x = 20;
+//     console.log(x);
+//     // Re-declaration is allowed
+
+//     x = 30;
+//     console.log(x);
+//     // Re-assignation is allowed
+// }
+
+// {
+//     let y = 40;
+//     // console.log(y);
+
+//     // let y = 50;
+//     // Re-declaration is not allowed using let keyword
+
+//     y = 50;
+//     console.log(y);
+//     // Re-assign is allowed using let keyword
+// }
+
 {
-    var x = 10;
-    console.log(x);
+    const z = 70;
+    console.log(z);
 
-    var x = 20;
-    console.log(x);
-    // Re-declaration allowed
+    // const z = 80;
+    // Re-declaration is not allowed
 
-    x = 30;
-    console.log(x);
-    // Re-assignation allowed
+    // z = 90;
+    // console.log(z);
+    // Re-assignation is not allowed
 }
 
-{
-    let y = 40;
-
-}
+/*
+Question: What are the key differences among var, let and const keywords?
+Answer: var, let and const keywords are used to declare variable in javascript language. But there are some key differences among var, let and const keywords. If we use var keyword then both re-declarion and re-assignation are allowed. If we use let keyword then re-declaration is allowed but re-assignation are not allowed. If we use const keyword then both re-declaration and re-assignation are not allowed.
+*/
